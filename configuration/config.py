74e54f94a3f87ffdafd372430082aa024f241bed
AmadouDiallo4 <diallobis2011@gmail.com>
2023-04-16 00:43:04 +0200

    add project

---
+++ configuration/config.py
@@ -0,0 +1,10 @@
+class TestData:
+    BROWSER = 'chrome'
+    #FIREFOX_EXECUTABLE_PATH = "drivers/geckodriver-linux/geckodriver"
+    #CHROME_EXECUTABLE_PATH = "drivers/chromedriver-linux/chromedriver"
+
+    URL = "https://opensource-demo.orangehrmlive.com/"
+    USERNAME = "Admin"
+    PASSWORD = "Admin123"
+
+    IMPLICIT_WAIT = 10
